133f91c7666a755dde36eb80dba6852c9a1d28f4
이영수 <yslee9311@gmail.com>
2024-08-13 21:19:22 +0900

    FEAT: Revenue Section 에서 선그래프 표출

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -94,6 +94,18 @@ const Ecommerce = () => {
 
                   <p className="text-gray-500 mt-1">Expense</p>
                 </div>
+
+                <div className="mt-5">
+                  <SparkLine
+                    currentColor="blue"
+                    id="line-sparkLine"
+                    type="Line"
+                    height="80px"
+                    width="250px"
+                    data={SparklineAreaData}
+                    color="blue"
+                  />
+                </div>
               </div>
             </div>
           </div>
