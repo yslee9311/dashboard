447d00d25836ffde150100da1a36329fc49b5cb0
이영수 <yslee9311@gmail.com>
2024-08-15 20:26:11 +0900

    FEAT: Revenue Section 에서 선그래프 하단 버튼 추가

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -106,6 +106,14 @@ const Ecommerce = () => {
                     color="blue"
                   />
                 </div>
+                <div className="mt-10">
+                  <Button
+                    color="white"
+                    bgColor="blue"
+                    text="Download Report"
+                    borderRadius="10px"
+                  />
+                </div>
               </div>
             </div>
           </div>
